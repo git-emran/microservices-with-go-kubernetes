5a666cc08f494d51eaeae120f0613013ad5b7278
Emran Hossain <151853372+git-emran@users.noreply.github.com>
2025-10-20 20:48:13 +0600

    Tip model and Ridefare model

---
+++ services/trip-service/internal/domain/trip.go
@@ -0,0 +1,17 @@
+package domain
+
+import "go.mongodb.org/mongo-driver/bson/primitive"
+
+type RideFareModel struct {
+	ID                primitive.ObjectID
+	UserID            string
+	PackageSlug       string
+	TotalPriceInCents float64
+}
+
+type TripModel struct {
+	ID       primitive.ObjectID
+	UserID   string
+	Status   string
+	RideFare RideFareModel
+}
